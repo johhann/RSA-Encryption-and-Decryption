b289a1f3bdbe14bdd1260f9a5061526e74b7345e
Yohannes Mekonnen <johannesm1219@gmail.com>
2022-09-24 12:09:09 +0300

    changes to encrypt and decrypt

--- Crypt.php
+++ Crypt.php
@@ -9,7 +9,7 @@ class Crypt
     public function __construct()
     {
         $this->publicKey = "-----BEGIN PUBLIC KEY-----\r\n" . chunk_split("Your-public-key"). "-----END PUBLIC KEY-----";
-        $this->privateKey = "-----BEGIN PRIVATE KEY-----\n" . chunk_split(config("Your-private-key")). "-----END PRIVATE KEY-----";
+        $this->privateKey = "-----BEGIN PRIVATE KEY-----\n" . chunk_split("Your-private-key"). "-----END PRIVATE KEY-----";
 
         $this->config = array([
             'config' => './openssl.cnf',
@@ -38,7 +38,7 @@ class Crypt
 
     public function encrypt($data)
     {
-        if (openssl_public_encrypt($data, $encrypted, $this->publicKey, OPENSSL_PKCS1_PADDING,))
+        if (openssl_public_encrypt($data, $encrypted, $this->publicKey, OPENSSL_PKCS1_PADDING))
             $data = base64_encode($encrypted);
         else
             throw new \Exception('Unable to encrypt data. Perhaps it is bigger than the key size?');
@@ -48,8 +48,8 @@ class Crypt
 
     public function decrypt($data)
     {
-        if (openssl_private_decrypt(base64_decode($data), $decrypted, $this->privateKey, OPENSSL_PKCS1_PADDING,))
-            $data = $decrypted;
+        if (openssl_private_decrypt(base64_decode($data), $decrypted, $this->privateKey, OPENSSL_PKCS1_PADDING))
+            $data = $decrypted; 
         else
             $data = '';
 
